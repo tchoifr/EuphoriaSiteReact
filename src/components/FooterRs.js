6e3f05232ba90949032b9fa43d92ce53a3308aa3
François Giorgi <77675039+tchoifr@users.noreply.github.com>
2022-04-26 15:56:11 +0200

    euphoria

---
+++ src/components/FooterRs.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+function FooterRs() {
+    return (
+        <div className='footerRs'>
+          
+        <a href='https://www.facebook.com/euphoriamarseille'><i className="fab fa-facebook"></i></a> 
+        <a href='https://www.instagram.com/euphoriamarseille/'><i className="fab fa-instagram"></i></a>
+     
+       
+<div className='textFooter' >
+<p>Restauration de 12H à 14H30 et de 19H à 22H30</p> 
+
+<p>Posibilité de manger des Tapas de 15H à 2H du matin !</p> 
+
+<p>Du Mardi au Mercredi : 10h00 - 00h00</p> 
+ <p>Du Jeudi au Samedi : 10h00 - 02h00</p> 
+ <p>Dimanche et lundi fermé </p>
+
+ </div>
+
+        </div>
+
+    )
+}
+
+export default FooterRs
---
+++ src/components/Presentation.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Presentation() {
+    return (
+        <div className='imageChef' data-aos="zoom-in">
+             <img className='img' src='/img/fond/calendrier.jpg' alt='calendrier' />
+            <h1>L'équipe</h1>
+            <img className='img' src='/img/equipe/chef.webp' alt='test' />
+            <h2>Océane ATTALI</h2>
+            <p>Pratiquement née sur scène, elle ne l'a jamais quittée ! Poussée par sa passion,
+                 elle passe son bac théâtre, entame le conservatoire national d'art dramatique de Marseille
+                  et travaille désormais au château de la Buzine en tant qu'auteure, metteuse en scène et interprète.
+                   Ayant travaillé dans la restauration durant 10 saisons sur la plage de la Pointe Rouge pour payer ses études,
+                    elle devient aussi fervente de ce milieu et du service à la clientèle qui se rapproche beaucoup
+                     pour elle d'une pièce de théâtre différente à chaque table. Océane décide alors de rassembler ses deux
+                      passions et d'ouvrir L'Euphoria : un café théâtre - restaurant où l'on peut régaler tous ses sens de 
+                      toutes sortes d'art. Mais son plus grand désir est également d'offrir un lieu dans lequel les artistes 
+                      peuvent s'entraîner, représenter leurs oeuvres et dans lequel les inconnus, les étrangers de passage
+                       ou la clientèle habituelle puissent se sentir chez soi, admirer un spectacle et apprécier un repas.</p>
+
+                    <h2>Dimitri PIERRE</h2> 
+                    <p>Passionné des goûts et des couleurs, il passe son BTS en hôtellerie et restauration option cuisine à Bonneveine, Marseille.
+                         Il gravit les échelons de commis de cuisine à Second au fil des saisons dans l'hôtel 5 étoiles de la Voile
+                          d'Or à Saint Jean Cap Ferrat avec comme Chef Georges Pelissier. Il a travaillé également à La Nautique
+                           ou à la Trattoria Monticelli ce qui lui a permis d'élargir ses connaissances culinaires. Sa spécialité : 
+                           le poisson, ce par quoi il a commencé puis il a fini par maîtriser parfaitement la cuisson de ses viandes,
+                            et se lance maintenant dans l'art de travailler le gibier. Sa passion : anoblir les produits bruts et ordinaires
+                             tels que le panais ou le butternut, il crée sans cesse de nouveaux plats à partir d'ingrédients que l'on pourrait 
+                             qualifier de "basiques". Il aime inventer et réinventer ce que nous avons l'habitude de manger afin d'explorer
+                              les combinaisons et nouvelles saveurs possibles. Féru de musique éléctronique.</p>  
+        </div>
+    )
+}
+
+export default Presentation
